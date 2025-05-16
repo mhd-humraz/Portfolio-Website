d2e20b21a9ea352770871448ec3d066c85177891
Muhammed Humraz <muhammadhamraz201@gmail.com>
2025-05-16 10:10:43 +0530

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -10,60 +10,62 @@ const Contact = () => {
           <div className="contact-box">
             <h4>Email</h4>
             <p>
-              <a href="mailto:example@mail.com" data-cursor="disable">
-                example@mail.com
+              <a href="mailto:muhammadhamraz201@mail.com" data-cursor="disable">
+                humraz@mail.com
               </a>
             </p>
             <h4>Phone</h4>
             <p>
-              <a href="tel:+919920782622" data-cursor="disable">
-                +91 99999 99999
+              <a href="tel:+917736863289" data-cursor="disable">
+                +91 77368 63289
               </a>
             </p>
           </div>
           <div className="contact-box">
-            <h4>Social</h4>
-            <a
-              href="https://github.com"
-              target="_blank"
-              data-cursor="disable"
-              className="contact-social"
-            >
-              Github <MdArrowOutward />
-            </a>
-            <a
-              href="https://www.linkedin.com"
-              target="_blank"
-              data-cursor="disable"
-              className="contact-social"
-            >
-              Linkedin <MdArrowOutward />
-            </a>
-            <a
-              href="https://x.com"
-              target="_blank"
-              data-cursor="disable"
-              className="contact-social"
-            >
-              Twitter <MdArrowOutward />
-            </a>
-            <a
-              href="https://www.instagram.com"
-              target="_blank"
-              data-cursor="disable"
-              className="contact-social"
-            >
-              Instagram <MdArrowOutward />
-            </a>
-          </div>
-          <div className="contact-box">
-            <h2>
-              Designed and Developed <br /> by <span>Moncy Yohannan</span>
-            </h2>
-            <h5>
-              <MdCopyright /> 2024
-            </h5>
-          </div>
+           
+  <h4>Social</h4>
+  <a
+    href="https://github.com/mhd-humraz"
+    target="_blank"
+    data-cursor="disable"
+    className="contact-social"
+  >
+    Github <MdArrowOutward />
+  </a>
+  <a
+    href="https://www.linkedin.com/in/mhd-humraz-283a5435b"
+    target="_blank"
+    data-cursor="disable"
+    className="contact-social"
+  >
+    LinkedIn <MdArrowOutward />
+  </a>
+  <a
+    href="https://x.com"  // You didn't provide a Twitter link; update this if you have one
+    target="_blank"
+    data-cursor="disable"
+    className="contact-social"
+  >
+    Twitter <MdArrowOutward />
+  </a>
+  <a
+    href="https://www.instagram.com/muhammed_humraz_11"
+    target="_blank"
+    data-cursor="disable"
+    className="contact-social"
+  >
+    Instagram <MdArrowOutward />
+  </a>
+</div>
+<div className="contact-box">
+  <h2>
+    Designed and Developed <br /> by <span>Humraz Leo</span>
+  </h2>
+  <h5>
+    <MdCopyright /> 2025
+  </h5>
+</div>
+
         </div>
       </div>
     </div>
