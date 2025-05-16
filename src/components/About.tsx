f81979b2ca7cda4682918555f6aa4e32cf4f2c66
Muhammed Humraz <muhammadhamraz201@gmail.com>
2025-05-16 09:57:51 +0530

    Update About.tsx

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -6,9 +6,12 @@ const About = () => {
       <div className="about-me">
         <h3 className="title">About Me</h3>
         <p className="para">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic quis
-          dolores numquam iusto Ratione earum ducimus autem id iure pariatur
-          dolorum quae maiores.
+        I'm Humraz, also known as Leo, a BCA student at MES M.K. Mackar Pillay College and the Event Planner of our Computer Science Department Association, BYTE BEGIN.
+          I'm passionate about technology, creativity, and leadership, with a strong interest in future tech, backend development, and AI integration.
+          I’ve organized major college events like Nexora, led a disaster management hackathon team, and run a small business called Origin Media,
+          which sells wall posters with a brand identity I designed myself. While not deeply into coding, I enjoy solving problems, designing posters using Canva,
+          drawing, and I'm planning to learn acoustic guitar. My past as an SPC commander shaped my leadership skills, and I prefer meaningful friendships over large
+          circles. I'm constantly exploring ways to combine tech and creativity to make a positive impact..
         </p>
       </div>
     </div>
