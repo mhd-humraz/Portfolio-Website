1b5c7a51d8e1972b27057d7f0d98cc64f3b3e602
Muhammed Humraz <muhammadhamraz201@gmail.com>
2025-05-16 10:16:07 +0530

    Update Landing.tsx

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,9 +9,9 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              MONCY
+              MUHAMMED
               <br />
-              <span>YOHANNAN</span>
+              <span>HUMRAZ</span>
             </h1>
           </div>
           <div className="landing-info">
