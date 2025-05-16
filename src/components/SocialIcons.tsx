03d5909fcf68be486d3e5753c2107de62b1fd91b
Muhammed Humraz <muhammadhamraz201@gmail.com>
2025-05-16 10:27:45 +0530

    Update SocialIcons.tsx

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -60,12 +60,12 @@ const SocialIcons = () => {
     <div className="icons-section">
       <div className="social-icons" data-cursor="icons" id="social">
         <span>
-          <a href="https://github.com" target="_blank">
+          <a href="https://github.com/mhd-humraz" target="_blank">
             <FaGithub />
           </a>
         </span>
         <span>
-          <a href="https://www.linkedin.com" target="_blank">
+          <a     href="https://www.linkedin.com/in/mhd-humraz-283a5435b" target="_blank">
             <FaLinkedinIn />
           </a>
         </span>
@@ -75,7 +75,7 @@ const SocialIcons = () => {
           </a>
         </span>
         <span>
-          <a href="https://www.instagram.com" target="_blank">
+          <a href="https://www.instagram.com/muhammed_humraz_11" target="_blank">
             <FaInstagram />
           </a>
         </span>
