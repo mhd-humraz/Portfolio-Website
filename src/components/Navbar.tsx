401a3474f849fe33b2a19d79696503e9f16a17fc
Muhammed Humraz <muhammadhamraz201@gmail.com>
2025-05-16 10:25:41 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -46,11 +46,11 @@ const Navbar = () => {
           Logo
         </a>
         <a
-          href="mailto:example@mail.com"
+          href="mailto:muhammadhamraz201@mail.com"
           className="navbar-connect"
           data-cursor="disable"
         >
-          example@mail.com
+          humraz@mail.com
         </a>
         <ul>
           <li>
